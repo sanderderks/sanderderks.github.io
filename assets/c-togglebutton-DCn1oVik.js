72b5d712a80d387c97bbd93a08999426cb07c509
Sander Derks <sander1derks@live.nl>
2026-04-12 14:56:40 +0200

    Update website

---
+++ assets/c-togglebutton-DCn1oVik.js
@@ -0,0 +1,120 @@
+import{Er as e,Hi as t,Li as n,Or as r,Qr as i,Vr as a,Xr as o,bt as s,it as c,kr as l,qr as u,s as d,si as f,t as p}from"./c-ripple-DQ_tvAzB.js";import{t as m}from"./c-baseeditableholder-gnYfTNtu.js";var h=d.extend({name:`togglebutton`,style:`
+    .p-togglebutton {
+        display: inline-flex;
+        cursor: pointer;
+        user-select: none;
+        overflow: hidden;
+        position: relative;
+        color: dt('togglebutton.color');
+        background: dt('togglebutton.background');
+        border: 1px solid dt('togglebutton.border.color');
+        padding: dt('togglebutton.padding');
+        font-size: 1rem;
+        font-family: inherit;
+        font-feature-settings: inherit;
+        transition:
+            background dt('togglebutton.transition.duration'),
+            color dt('togglebutton.transition.duration'),
+            border-color dt('togglebutton.transition.duration'),
+            outline-color dt('togglebutton.transition.duration'),
+            box-shadow dt('togglebutton.transition.duration');
+        border-radius: dt('togglebutton.border.radius');
+        outline-color: transparent;
+        font-weight: dt('togglebutton.font.weight');
+    }
+
+    .p-togglebutton-content {
+        display: inline-flex;
+        flex: 1 1 auto;
+        align-items: center;
+        justify-content: center;
+        gap: dt('togglebutton.gap');
+        padding: dt('togglebutton.content.padding');
+        background: transparent;
+        border-radius: dt('togglebutton.content.border.radius');
+        transition:
+            background dt('togglebutton.transition.duration'),
+            color dt('togglebutton.transition.duration'),
+            border-color dt('togglebutton.transition.duration'),
+            outline-color dt('togglebutton.transition.duration'),
+            box-shadow dt('togglebutton.transition.duration');
+    }
+
+    .p-togglebutton:not(:disabled):not(.p-togglebutton-checked):hover {
+        background: dt('togglebutton.hover.background');
+        color: dt('togglebutton.hover.color');
+    }
+
+    .p-togglebutton.p-togglebutton-checked {
+        background: dt('togglebutton.checked.background');
+        border-color: dt('togglebutton.checked.border.color');
+        color: dt('togglebutton.checked.color');
+    }
+
+    .p-togglebutton-checked .p-togglebutton-content {
+        background: dt('togglebutton.content.checked.background');
+        box-shadow: dt('togglebutton.content.checked.shadow');
+    }
+
+    .p-togglebutton:focus-visible {
+        box-shadow: dt('togglebutton.focus.ring.shadow');
+        outline: dt('togglebutton.focus.ring.width') dt('togglebutton.focus.ring.style') dt('togglebutton.focus.ring.color');
+        outline-offset: dt('togglebutton.focus.ring.offset');
+    }
+
+    .p-togglebutton.p-invalid {
+        border-color: dt('togglebutton.invalid.border.color');
+    }
+
+    .p-togglebutton:disabled {
+        opacity: 1;
+        cursor: default;
+        background: dt('togglebutton.disabled.background');
+        border-color: dt('togglebutton.disabled.border.color');
+        color: dt('togglebutton.disabled.color');
+    }
+
+    .p-togglebutton-label,
+    .p-togglebutton-icon {
+        position: relative;
+        transition: none;
+    }
+
+    .p-togglebutton-icon {
+        color: dt('togglebutton.icon.color');
+    }
+
+    .p-togglebutton:not(:disabled):not(.p-togglebutton-checked):hover .p-togglebutton-icon {
+        color: dt('togglebutton.icon.hover.color');
+    }
+
+    .p-togglebutton.p-togglebutton-checked .p-togglebutton-icon {
+        color: dt('togglebutton.icon.checked.color');
+    }
+
+    .p-togglebutton:disabled .p-togglebutton-icon {
+        color: dt('togglebutton.icon.disabled.color');
+    }
+
+    .p-togglebutton-sm {
+        padding: dt('togglebutton.sm.padding');
+        font-size: dt('togglebutton.sm.font.size');
+    }
+
+    .p-togglebutton-sm .p-togglebutton-content {
+        padding: dt('togglebutton.content.sm.padding');
+    }
+
+    .p-togglebutton-lg {
+        padding: dt('togglebutton.lg.padding');
+        font-size: dt('togglebutton.lg.font.size');
+    }
+
+    .p-togglebutton-lg .p-togglebutton-content {
+        padding: dt('togglebutton.content.lg.padding');
+    }
+
+    .p-togglebutton-fluid {
+        width: 100%;
+    }
+`,classes:{root:function(e){var t=e.instance,n=e.props;return[`p-togglebutton p-component`,{"p-togglebutton-checked":t.active,"p-invalid":t.$invalid,"p-togglebutton-fluid":n.fluid,"p-togglebutton-sm p-inputfield-sm":n.size===`small`,"p-togglebutton-lg p-inputfield-lg":n.size===`large`}]},content:`p-togglebutton-content`,icon:`p-togglebutton-icon`,label:`p-togglebutton-label`}}),g={name:`BaseToggleButton`,extends:m,props:{onIcon:String,offIcon:String,onLabel:{type:String,default:`Yes`},offLabel:{type:String,default:`No`},readonly:{type:Boolean,default:!1},tabindex:{type:Number,default:null},ariaLabelledby:{type:String,default:null},ariaLabel:{type:String,default:null},size:{type:String,default:null},fluid:{type:Boolean,default:null}},style:h,provide:function(){return{$pcToggleButton:this,$parentInstance:this}}};function _(e){"@babel/helpers - typeof";return _=typeof Symbol==`function`&&typeof Symbol.iterator==`symbol`?function(e){return typeof e}:function(e){return e&&typeof Symbol==`function`&&e.constructor===Symbol&&e!==Symbol.prototype?`symbol`:typeof e},_(e)}function v(e,t,n){return(t=y(t))in e?Object.defineProperty(e,t,{value:n,enumerable:!0,configurable:!0,writable:!0}):e[t]=n,e}function y(e){var t=b(e,`string`);return _(t)==`symbol`?t:t+``}function b(e,t){if(_(e)!=`object`||!e)return e;var n=e[Symbol.toPrimitive];if(n!==void 0){var r=n.call(e,t);if(_(r)!=`object`)return r;throw TypeError(`@@toPrimitive must return a primitive value.`)}return(t===`string`?String:Number)(e)}var x={name:`ToggleButton`,extends:g,inheritAttrs:!1,emits:[`change`],methods:{getPTOptions:function(e){return(e===`root`?this.ptmi:this.ptm)(e,{context:{active:this.active,disabled:this.disabled}})},onChange:function(e){!this.disabled&&!this.readonly&&(this.writeValue(!this.d_value,e),this.$emit(`change`,e))},onBlur:function(e){var t,n;(t=(n=this.formField).onBlur)==null||t.call(n,e)}},computed:{active:function(){return this.d_value===!0},hasLabel:function(){return s(this.onLabel)&&s(this.offLabel)},label:function(){return this.hasLabel?this.d_value?this.onLabel:this.offLabel:`\xA0`},dataP:function(){return c(v({checked:this.active,invalid:this.$invalid},this.size,this.size))}},directives:{ripple:p}},S=[`tabindex`,`disabled`,`aria-pressed`,`aria-label`,`aria-labelledby`,`data-p-checked`,`data-p-disabled`,`data-p`],C=[`data-p`];function w(s,c,d,p,m,h){var g=i(`ripple`);return f((u(),l(`button`,a({type:`button`,class:s.cx(`root`),tabindex:s.tabindex,disabled:s.disabled,"aria-pressed":s.d_value,onClick:c[0]||=function(){return h.onChange&&h.onChange.apply(h,arguments)},onBlur:c[1]||=function(){return h.onBlur&&h.onBlur.apply(h,arguments)}},h.getPTOptions(`root`),{"aria-label":s.ariaLabel,"aria-labelledby":s.ariaLabelledby,"data-p-checked":h.active,"data-p-disabled":s.disabled,"data-p":h.dataP}),[e(`span`,a({class:s.cx(`content`)},h.getPTOptions(`content`),{"data-p":h.dataP}),[o(s.$slots,`default`,{},function(){return[o(s.$slots,`icon`,{value:s.d_value,class:n(s.cx(`icon`))},function(){return[s.onIcon||s.offIcon?(u(),l(`span`,a({key:0,class:[s.cx(`icon`),s.d_value?s.onIcon:s.offIcon]},h.getPTOptions(`icon`)),null,16)):r(``,!0)]}),e(`span`,a({class:s.cx(`label`)},h.getPTOptions(`label`)),t(h.label),17)]})],16,C)],16,S)),[[g]])}x.render=w;export{x as t};
